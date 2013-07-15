48f2163a8b1527d7712bab2bdfa156e212089f7d
Vladimir Gnatiuk <vovagn@ya.ru>
2013-07-15 15:12:22 +0300

    initial commit

---
+++ iCarouselExampleViewController.m
@@ -0,0 +1,325 @@
+//
+//  iCarouselExampleViewController.m
+//  iCarouselExample
+//
+//  Created by Nick Lockwood on 03/04/2011.
+//  Copyright 2011 Charcoal Design. All rights reserved.
+//
+
+#import "iCarouselExampleViewController.h"
+
+
+@interface iCarouselExampleViewController () <UIActionSheetDelegate>
+
+@property (nonatomic, assign) BOOL wrap;
+@property (nonatomic, retain) NSMutableArray *items;
+
+@end
+
+
+@implementation iCarouselExampleViewController
+
+@synthesize carousel;
+@synthesize navItem;
+@synthesize orientationBarItem;
+@synthesize wrapBarItem;
+@synthesize wrap;
+@synthesize items;
+
+- (void)setUp
+{
+    //set up data
+    wrap = YES;
+    self.items = [NSMutableArray array];
+    for (int i = 0; i < 100; i++)
+    {
+        [items addObject:[NSNumber numberWithInt:i]];
+    }
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+    {
+        [self setUp];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self = [super initWithCoder:aDecoder]))
+    {
+        [self setUp];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    //it's a good idea to set these to nil here to avoid
+    //sending messages to a deallocated viewcontroller
+    carousel.delegate = nil;
+    carousel.dataSource = nil;
+    
+    [carousel release];
+    [navItem release];
+    [orientationBarItem release];
+    [wrapBarItem release];
+    [items release];
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //configure carousel
+    carousel.type = iCarouselTypeCustom;
+	carousel.vertical = YES;
+    navItem.title = @"Custom";
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    self.carousel = nil;
+    self.navItem = nil;
+    self.orientationBarItem = nil;
+    self.wrapBarItem = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+- (IBAction)switchCarouselType
+{
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"Select Carousel Type"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                         destructiveButtonTitle:nil
+                                              otherButtonTitles:@"Linear", @"Rotary", @"Inverted Rotary", @"Cylinder", @"Inverted Cylinder", @"Wheel", @"Inverted Wheel", @"CoverFlow", @"CoverFlow2", @"Time Machine", @"Inverted Time Machine", @"Custom", nil];
+    [sheet showInView:self.view];
+    [sheet release];
+}
+
+- (IBAction)toggleOrientation
+{
+    //carousel orientation can be animated
+    [UIView beginAnimations:nil context:nil];
+    carousel.vertical = !carousel.vertical;
+    [UIView commitAnimations];
+    
+    //update button
+    orientationBarItem.title = carousel.vertical? @"Vertical": @"Horizontal";
+}
+
+- (IBAction)toggleWrap
+{
+    wrap = !wrap;
+    wrapBarItem.title = wrap? @"Wrap: ON": @"Wrap: OFF";
+    [carousel reloadData];
+}
+
+- (IBAction)insertItem
+{
+    NSInteger index = MAX(0, carousel.currentItemIndex);
+    [items insertObject:[NSNumber numberWithInt:carousel.numberOfItems] atIndex:index];
+    [carousel insertItemAtIndex:index animated:YES];
+}
+
+- (IBAction)removeItem
+{
+    if (carousel.numberOfItems > 0)
+    {
+        NSInteger index = carousel.currentItemIndex;
+        [items removeObjectAtIndex:index];
+        [carousel removeItemAtIndex:index animated:YES];
+    }
+}
+
+#pragma mark -
+#pragma mark UIActionSheet methods
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex >= 0)
+    {
+        //map button index to carousel type
+        iCarouselType type = buttonIndex;
+        
+        //carousel can smoothly animate between types
+        [UIView beginAnimations:nil context:nil];
+        carousel.type = type;
+        [UIView commitAnimations];
+        
+        //update title
+        navItem.title = [actionSheet buttonTitleAtIndex:buttonIndex];
+    }
+}
+
+#pragma mark -
+#pragma mark iCarousel methods
+
+- (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+    return [items count];
+}
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
+{
+    UILabel *label = nil;
+    
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        view = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)] autorelease];
+        ((UIImageView *)view).image = [UIImage imageNamed:@"page.png"];
+        view.contentMode = UIViewContentModeCenter;
+        label = [[[UILabel alloc] initWithFrame:view.bounds] autorelease];
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = UITextAlignmentCenter;
+        label.font = [label.font fontWithSize:50];
+        label.tag = 1;
+        [view addSubview:label];
+    }
+    else
+    {
+        //get a reference to the label in the recycled view
+        label = (UILabel *)[view viewWithTag:1];
+    }
+    
+    //set item label
+    //remember to always set any properties of your carousel item
+    //views outside of the `if (view == nil) {...}` check otherwise
+    //you'll get weird issues with carousel item content appearing
+    //in the wrong place in the carousel
+    label.text = [[items objectAtIndex:index] stringValue];
+    
+    return view;
+}
+
+- (NSUInteger)numberOfPlaceholdersInCarousel:(iCarousel *)carousel
+{
+    //note: placeholder views are only displayed on some carousels if wrapping is disabled
+    return 2;
+}
+
+- (UIView *)carousel:(iCarousel *)carousel placeholderViewAtIndex:(NSUInteger)index reusingView:(UIView *)view
+{
+    UILabel *label = nil;
+    
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        //don't do anything specific to the index within
+        //this `if (view == nil) {...}` statement because the view will be
+        //recycled and used with other index values later
+        view = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)] autorelease];
+        ((UIImageView *)view).image = [UIImage imageNamed:@"page.png"];
+        view.contentMode = UIViewContentModeCenter;
+        
+        label = [[[UILabel alloc] initWithFrame:view.bounds] autorelease];
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = UITextAlignmentCenter;
+        label.font = [label.font fontWithSize:50.0f];
+        label.tag = 1;
+        [view addSubview:label];
+    }
+    else
+    {
+        //get a reference to the label in the recycled view
+        label = (UILabel *)[view viewWithTag:1];
+    }
+    
+    //set item label
+    //remember to always set any properties of your carousel item
+    //views outside of the `if (view == nil) {...}` check otherwise
+    //you'll get weird issues with carousel item content appearing
+    //in the wrong place in the carousel
+    label.text = (index == 0)? @"[": @"]";
+    
+    return view;
+}
+
+- (CATransform3D)carousel:(iCarousel *)_carousel itemTransformForOffset:(CGFloat)offset baseTransform:(CATransform3D)transform
+{
+    CGFloat height = 200.0;
+	
+	CGFloat tilt = 1.0/sqrt(2.3);
+	CGFloat spacing = 1.;
+	
+	float lineSpace = 5.0;
+	float dz = 0.0;
+	float dy = 0.0;
+	float strech = 0.1;
+	float angle = 0;
+	
+	if (offset > 1.0)
+	{
+		dz = 1000;
+	}
+	else if (offset > 0)
+	{
+		angle = -3 * sinf(M_PI * offset/3.0);
+		dz = -(angle) * 100;
+		dy = -(angle) * 10;
+	}
+	else if(offset < -lineSpace)
+	{
+		dz = (offset + lineSpace);
+		dy = -(offset + lineSpace) * 100;
+		angle = 0;
+	}
+	
+	transform = CATransform3DTranslate(transform, 0.0f, strech*(offset * height / tilt) + dy + (spacing/2)*height, strech*(offset * height / tilt) + dz);
+	
+	transform = CATransform3DRotate(transform, angle * M_PI*0.1, 1.0f, 0.0f, 0.0f);
+	
+	return transform;
+}
+
+- (CGFloat)carousel:(iCarousel *)_carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value
+{
+	switch (option)
+	{
+		case iCarouselOptionFadeMin:
+			return -4.0;
+		case iCarouselOptionFadeMax:
+			return 0.01;
+		case iCarouselOptionFadeRange:
+			return 3.0;
+		case iCarouselOptionTilt:
+			return 0.0;
+		case iCarouselOptionSpacing:
+			return 0.5;
+		case iCarouselOptionWrap:
+			return 0.0;
+		case iCarouselOptionVisibleItems:
+			return 13.0;
+		default:
+			return value;
+	}
+}
+
+- (CGFloat)carousel:(iCarousel *)carousel itemAlphaForOffset:(CGFloat)offset
+{
+	//set opacity based on distance from camera
+	return 1.0f - fminf(fmaxf(offset, 0.0f), 1.0f);
+}
+
+#pragma mark -
+#pragma mark iCarousel taps
+
+- (void)carousel:(iCarousel *)carousel didSelectItemAtIndex:(NSInteger)index
+{
+    NSNumber *item = [self.items objectAtIndex:index];
+    NSLog(@"Tapped view number: %@", item);
+}
+
+@end
